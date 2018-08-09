a66c907264fb0944e7bb91bdb2ecfe5084485732
Gourav Roy <42024566+gourroy21@users.noreply.github.com>
2018-08-09 17:27:22 +0530

    Update LoanException.java

--- src/com/cg/loan/exception/LoanException.java
+++ src/com/cg/loan/exception/LoanException.java
@@ -1,14 +1,10 @@
 package com.cg.loan.exception;
 
-<<<<<<< HEAD
 public class LoanException extends Exception{
 
 	public LoanException(String msg)
 	{
 		super(msg);
 	}
-=======
-public class LoanException {
 
->>>>>>> eddb28e9aefda37c2842440210897810564d625f
 }
