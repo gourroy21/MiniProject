7c33a697f8ddc8aafd491328844b37ca9692f6b0
Gourav Roy <42024566+gourroy21@users.noreply.github.com>
2018-08-09 17:30:24 +0530

    Update DBUtil.java

--- src/com/cg/loan/util/DBUtil.java
+++ src/com/cg/loan/util/DBUtil.java
@@ -1,10 +1,8 @@
 package com.cg.loan.util;
 
 import java.io.FileInputStream;
-<<<<<<< HEAD
+
 import java.io.FileNotFoundException;
-=======
->>>>>>> eddb28e9aefda37c2842440210897810564d625f
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,7 +10,6 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBUtil {
-<<<<<<< HEAD
 public static Connection getConnection() {
 		
 		String driver="";
@@ -56,41 +53,5 @@ public static Connection getConnection() {
 		return conn;
 		
 		
-=======
-	public static Connection getConnection() {
-			String driver="";
-			String url="";
-			String username="";
-			String passwd="";
-			Connection connection=null;
-			try {
-				FileInputStream fin=new FileInputStream("DBConfig.properties");
-				Properties prop=new Properties();
-				prop.load(fin);
-				driver=prop.getProperty("driver");
-				url=prop.getProperty("url");
-				username=prop.getProperty("username");
-				passwd=prop.getProperty("passwd");
-				
-				Class.forName(driver);
-				connection=DriverManager.getConnection(url, username, passwd);
-				
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return connection;
-			
-	}
-	public static void main(String[] args) {
-		getConnection();
->>>>>>> eddb28e9aefda37c2842440210897810564d625f
-	}
-
+}
 }
